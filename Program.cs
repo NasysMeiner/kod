00ca1be1ef75b7bd0c065146658ff2dd587fcf21
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-15 21:17:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool alive = true;
+            int hp = 50;
+            float damage = 7.76f;
+            double healing = 7.5;
+            byte money = 255;
+            sbyte spentMoney = -128;
+            short armor = 100;
+            ushort damageArmor = 100;
+            char symbol = '*';
+            string str = "Привет!";
+        }
+    }
+}
