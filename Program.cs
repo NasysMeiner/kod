729110047f98721b6fb907c08a3ba35a26eb8ca7
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-15 20:30:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bl = true;
+            int i = 7;
+            float f = 7f;
+            double db = 7.0;
+            byte b = 255;
+            sbyte sb = -128;
+            short sh = -100;
+            ushort us = 100;
+            char ch = '*';
+            string str = "Привет!";
+        }
+    }
+}
