7264b74ea0ee49ff5ddf2cbab24720174ae69dfa
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-16 19:01:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 30);
+            int sum1 = 0;
+            int sum2 = 0;
+            int numberOne = 3;
+            int numberTwo = 5;
+
+            for (int i = 0; i <= number; i += numberOne)
+            {
+                sum1 += i;
+            }
+
+            for (int i = 0; i <= number; i += numberTwo)
+            {
+                sum2 += i;
+            }
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+
+            Console.ReadLine();
+        }
+    }
+}
