5f0be60f8cd8195b1b2b7faa82645156f7f544db
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-17 14:52:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int min = 1;
+            int max = 28;
+            int number = random.Next(min, max);
+            int upperBound = 1000;
+            int amountOfNumbers = 0;
+
+            for (int i = 0; i < upperBound; i += number)
+            {
+                if (i > 100)
+                {
+                    amountOfNumbers++;
+                }
+            }
+            Console.WriteLine($"Кол-во чисел кратных: {number} равно: {amountOfNumbers}.");
+            Console.ReadLine();
+        }
+    }
+}
