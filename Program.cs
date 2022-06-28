0721557fe4d606786cc3671acaf7b5f99621cbed
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-28 12:31:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw drawPlayer = new Draw();
+            Player player = new Player(10, 20);
+
+            drawPlayer.DrawPlayer(player.X, player.Y);
+            Console.ReadLine();
+        }
+    }
+
+    class Player
+    {
+        public Player(int positionX, int positionY)
+        {
+            X = positionX;
+            Y = positionY;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+    }
+
+    class Draw
+    {
+        public void DrawPlayer(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("%");
+        }
+    }
+}
